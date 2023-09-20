9a7631a57206d95bfecbab30b7106f70757f45a9
szucsv <b2szuvil@vasvari.org>
2023-09-20 08:38:03 +0200

    2023.09.20

---
+++ 01 - Kiírás/Feladat 01/main.py
@@ -0,0 +1,4 @@
+model:str = "Torino Cobra SportsRoof"
+productionYear:int = 1970
+
+print(f"A Ford autógyár {productionYear}-ben gyártotta le a {model} modellt")
---
+++ 01 - Kiírás/Feladat 02/main.py
@@ -0,0 +1,5 @@
+sorozat:str = "i7"
+tipus:int =8700
+frekvencia:float =4.2
+
+print(f"Az INTEL{sorozat} sorozatjelzésű mikroprocesszor {tipus} típusának munkafrekenciája {frekvencia}")
