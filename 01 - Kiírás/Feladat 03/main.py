b7d1708a3f251bc82e7a2e4772882450f9481808
szucsv <b2szuvil@vasvari.org>
2023-09-21 18:46:33 +0200

    Házi

---
+++ 01 - Kiírás/Feladat 03/main.py
@@ -0,0 +1,6 @@
+year:int = 1644
+year2:int = 1368
+lenght:int = 7200
+chinese:str = "Kínai Nagyfal"
+dynasty:str = "Ming"
+print(f"A {dynasty} dinasztia {year} és {year2} közt építette a ma látható {chinese} {lenght}km hosszú szakaszát.")
---
+++ 01 - Kiírás/Feladat 04/main.py
@@ -0,0 +1,6 @@
+manufactor:str = "Sukhoj"
+model:str = "SU-35S"
+year:int = 2008
+mach:float = 2.4
+speed:str = "m/s"
+print(f"oroszország legmodernebb vadászreülőgépe a {year}-ban bemutatott {manufactor} gyártotta {model} amely képes a {mach} Mach sebességre, ami 720{speed}-nak felel meg.")
